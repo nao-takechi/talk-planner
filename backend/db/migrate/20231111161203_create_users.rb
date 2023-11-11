2e2d4857deab731ace7fd842a034a82af0e9f81c
武智奈央 <nao@Nao-TakechiMacBook-Pro.local>
2023-11-12 01:32:31 +0900

    Revert "[tmp-edit] create table"
    This reverts commit 8317dea70cbfe1753a8f34747ea1816e7b4e9c6a.

--- backend/db/migrate/20231111161203_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[7.1]
-  def change
-    create_table :users do |t|
-      t.string :line_id
-      t.integer :message_send_time
-      t.timestamps
-    end
-  end
-end
