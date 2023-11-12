8b37cce1e2d20db4b4f0ca66a68875eeda6d0266
武智奈央 <nao@Nao-TakechiMacBook-Pro.local>
2023-11-12 20:30:56 +0900

    ディレクトリ整理

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   define: { 'process.env': {} },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, 'frontend')
     }
   },
   server: {
